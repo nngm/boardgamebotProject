c3c829757ef80d83e7ee289fefe0b369ccfacf10
nngm <andrea1010@naver.com>
2021-01-06 16:35:03 +0900

    set bot activity

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import asyncio
 import discord
 from discord.ext import commands
 
-command_prefix = '/'
-bot = commands.Bot(command_prefix=command_prefix)
+basic_command_prefix = '/'
+
+def get_command_prefix(bot, message):
+    return basic_command_prefix
+
+bot = commands.Bot(command_prefix=get_command_prefix)
 
 # def console_input():
 #     while True:
@@ -16,21 +20,17 @@ bot = commands.Bot(command_prefix=command_prefix)
 
 @bot.event
 async def on_ready():
-    activity = discord.CustomActivity('basic prefix is ' + command_prefix)
-    await bot.change_presence(activity=activity)    # not working now
+    # custom activity for bots are not available now
+    activity = discord.Game('basic prefix is ' + basic_command_prefix)
+    await bot.change_presence(activity=activity)    
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
     print('------')
 
-    # await console_input()
-
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
-async def main():
+if __name__ == '__main__':
     bot.run(__token__.get_token())
-
-if __name__ == "__main__":
-    asyncio.run(main())
