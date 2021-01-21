cb98848319dd3787582c115525370846dcfcdc3a
nngm <andrea1010@naver.com>
2021-01-21 16:49:27 +0900

    make attributes

--- omok.py
+++ omok.py
@@ -1,3 +1,24 @@
+class Status:
+    pending = 1
+    playing = 2
+    stopped = 3
+    
 class Omok:
-    def __init__(self):
-        return
+
+    class Rule:
+        free = 1
+        standard = 2
+        omok = 3
+        renju = 4
+        RIF = 5
+        soosyrv = 6
+
+    def __init__(self, id1, id2):
+        self.status = Status.pending
+        self.move = 0
+        self.player1 = id1
+        self.player2 = id2
+        self.board_size = 15
+        self.rule = Omok.Rule.renju
+        self.black = self.player1
+        self.white = self.player2
