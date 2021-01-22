9dfb8caf7c6c8a2a9b423c37e05dd39a8dbd75f4
nngm <andrea1010@naver.com>
2021-01-22 17:19:59 +0900

    merge omok.py to game.py

--- game.py
+++ game.py
@@ -1,12 +1,30 @@
-import abc
+# import abc
 
-class Game(metaclass=abc.ABCMeta):
+class Game:
     class Status:
-        pending = 1
-        playing = 2
-        stopped = 3
+        pending = 0
+        playing = 1
+        stopped = 2
 
-    @abc.abstractmethod
     def __init__(self, players):
         self.status = Game.Status.pending
         self.players = players
+
+class Omok(Game):
+
+    class Rule:
+        free = 0
+        standard = 1
+        omok = 2
+        renju = 3
+        RIF = 4
+        soosyrv = 5
+
+    def __init__(self, id1, id2):
+        super.__init__([id1, id2])
+        self.move = 0
+        self.moves = []
+        self.board_size = 15
+        self.rule = Omok.Rule.renju
+        self.black = self.players[0]
+        self.white = self.players[1]
